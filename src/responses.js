aacfa337ca960cf71217f8e0883aab4c78e43318
Benjamin Kleynhans <bxk8027@rit.edu>
2020-02-06 00:40:08 -0500

    Changed arrow functions to normal functions because ESLint was confused and complained about lengths of more than 100 characters for functions that did not have more than 100 characters per line

--- src/responses.js
+++ src/responses.js
@@ -1,39 +1,39 @@
 const messages = {
   200: {
     success: {
-      message: 'This is a successful response',
+      message: "This is a successful response"
     },
     badRequest: {
-      message: 'This request has the required parameters',
+      message: "This request has the required parameters"
     },
     unauthorized: {
-      message: 'You have successfully viewed the content',
+      message: "You have successfully viewed the content"
     },
   },
   400: {
-    id: 'badRequest',
-    message: 'Missing valid query parameter set to true',
+    id: "badRequest",
+    message: "Missing valid query parameter set to true"
   },
   401: {
-    id: 'unauthorized',
-    message: 'Missing loggedIn query parameter set to yes',
+    id: "unauthorized",
+    message: "Missing loggedIn query parameter set to yes"
   },
   403: {
-    id: 'forbidden',
-    message: 'You do not have access to this content.',
+    id: "forbidden",
+    message: "You do not have access to this content."
   },
   404: {
-    id: 'notFound',
-    message: 'The page you are looking for was not found.',
+    id: "notFound",
+    message: "The page you are looking for was not found."
   },
   500: {
-    id: 'internalError',
-    message: 'Internal Server Error. Something went wrong.',
+    id: "internalError",
+    message: "Internal Server Error. Something went wrong."
   },
   501: {
-    id: 'notImplemented',
-    message: 'A get request for this page has not been implemented yet.  Check again later for updated content.',
-  },
+    id: "notImplemented",
+    message: "A get request for this page has not been implemented yet.  Check again later for updated content."
+  }
 };
 
 const toXMLString = (messageId, url = null) => {
@@ -54,7 +54,8 @@ const toXMLString = (messageId, url = null) => {
 };
 
 const respond = (request, response, status, params) => {
-  const tempParams = params;
+
+  let tempParams = params;
   let typedResponse;
 
   if (status === 200) {
@@ -64,21 +65,23 @@ const respond = (request, response, status, params) => {
       typedResponse = JSON.stringify(messages[status][tempParams.url.substring(1)]);
       tempParams.acceptHeader = 'application/json';
     }
-  } else if (tempParams.acceptHeader === 'text/xml') {
-    typedResponse = toXMLString(status);
   } else {
-    typedResponse = JSON.stringify(messages[status]);
-    tempParams.acceptHeader = 'application/json';
+    if (tempParams.acceptHeader === 'text/xml') {
+      typedResponse = toXMLString(status);
+    } else {
+      typedResponse = JSON.stringify(messages[status]);
+      tempParams.acceptHeader = 'application/json';
+    }
   }
 
-  response.writeHead(status, { 'Content-Type': tempParams.acceptHeader });
+  response.writeHead(status, {'Content-Type': tempParams.acceptHeader});
   response.write(typedResponse);
   response.end();
 };
 
 const respondMeta = (request, response, status, params) => {
   const headers = {
-    'Content-Type': params.acceptHeader,
+    'Content-Type': params.acceptHeader
   };
 
   response.writeHead(status, headers);
@@ -89,7 +92,9 @@ const success = (request, response, params) => {
   respond(request, response, 200, params);
 };
 
-const successMeta = (request, response, params) => respondMeta(request, response, 200, params);
+const successMeta = (request, response, params) => {
+  return respondMeta(request, response, 200, params);
+};
 
 const badRequest = (request, response, params) => {
   let returnValue;
@@ -103,7 +108,9 @@ const badRequest = (request, response, params) => {
   return returnValue;
 };
 
-const badRequestMeta = (request, response, params) => respondMeta(request, response, 400, params);
+const badRequestMeta = (request, response, params) => {
+  return respondMeta(request, response, 400, params);
+};
 
 const unauthorized = (request, response, params) => {
   let returnValue;
@@ -117,41 +124,41 @@ const unauthorized = (request, response, params) => {
   return returnValue;
 };
 
-const unauthorizedMeta = (request, response, params) => {
-  respondMeta(request, response, 401, params);
-};
+function unauthorizedMeta (request, response, params) {
+  return respondMeta(request, response, 401, params);
+}
 
-const forbidden = (request, response, params) => {
-  respond(request, response, 403, params);
-};
+function forbidden (request, response, params) {
+  return respond(request, response, 403, params);
+}
 
-const forbiddenMeta = (request, response, params) => {
-  respondMeta(request, response, 403, params);
-};
+function forbiddenMeta (request, response, params) {
+  return respondMeta(request, response, 403, params);
+}
 
-const internal = (request, response, params) => {
-  respond(request, response, 500, params);
-};
+function internal(request, response, params) {
+  return respond(request, response, 500, params);
+}
 
-const internalMeta = (request, response, params) => {
-  respondMeta(request, response, 500, params);
-};
+function internalMeta(request, response, params) {
+  return respondMeta(request, response, 500, params);
+}
 
-const notImplemented = (request, response, params) => {
-  respond(request, response, 501, params);
-};
+function notImplemented(request, response, params) {
+  return respond(request, response, 501, params);
+}
 
-const notImplementedMeta = (request, response, params) => {
-  respondMeta(request, response, 501, params);
-};
+function notImplementedMeta(request, response, params) {
+  return respondMeta(request, response, 501, params);
+}
 
-const notFound = (request, response, params) => {
-  respond(request, response, 404, params);
-};
+function notFound(request, response, params) {
+  return respond(request, response, 404, params);
+}
 
-const notFoundMeta = (request, response, params) => {
-  respondMeta(request, response, 404, params);
-};
+function notFoundMeta(request, response, params) {
+  return respondMeta(request, response, 404, params);
+}
 
 module.exports = {
   success,
